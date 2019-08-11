844013c0320512a0815c694217dac14a742885d0
Grub4K <simonben@abwesend.de>
2019-08-11 23:16:32 +0200

    Update main.py
    - removed unneccesary imports
    - cleared temp holder of regex (24)
    - ddded Fritz!Box model detection
    - padded integer for reset countdown
    - removed some whitespace

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fritz_postconfig import Connection
-from random import randint
 import time
 import subprocess
-import json
 import requests
+
 
 
 def wait_for_connection(host):
@@ -16,30 +15,24 @@ def wait_for_connection(host):
         time.sleep(1)
     print('')
 
-
 host = input('Please enter host ({ENTER} for fritz.box): ')
 if not host:
     host = 'fritz.box'
 wait_for_connection(host)
-'<modelDescription>([\s\S])</modelDescription>'
 host = 'http://' + host
-
-# Detect fritzbox
-
-
+# Detect fritzbox model
+resp = requests.get(host + ':49000/tr64desc.xml')
+result = re.findall('<modelName>(.*?)<\/modelName>', resp.text)
+modelname, *_ = result
 # Setup connection
-
-
-fritz = Connection(host, 'FRITZ!Box FON WLAN')
+fritz = Connection(host, modelname)
+# reset
 fritz.reset()
 for i in range(100,-1,-1):
-    print(f"\r restarting {i}s left",end="")
+    print(f"\r restarting {i:>3}s left", end="")
     time.sleep(1)
-
 print()
-
 fritz.enableExpertMode()
 fritz.setupInternet()
 fritz.login()
-
 fritz.wifiSetup()
