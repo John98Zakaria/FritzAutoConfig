e42e1bbe9f5970692a5ebafc7e597092a6aa38d9
Grub4K <simonben@abwesend.de>
2019-08-15 20:31:05 +0200

    Reworked main, added MAC display

--- fritz_postconfig/connection.py
+++ fritz_postconfig/connection.py
@@ -4,6 +4,9 @@ from xml.etree import ElementTree as ET
 import json
 import requests
 import time
+import re
+import subprocess
+import urllib
 from random import randint
 
 class Connection:
@@ -11,31 +14,87 @@ class Connection:
         self.host = host
         self._sid = None
         self.model = model
+
+    def setup(self, *args):
         with open(self.model + '.json', 'r') as f:
             self.json = json.load(f)
+        if not self.reset():
+            # reset failed, retry with different model
+            return False
+        self.login()
+        self.enableExpertMode()
+        self.setupInternet()
+        self.wifiSetup()
+        self.getMac()
+        return True
 
     def call_api(self, dict):
         data = dict['data']
         route = dict['route']
         if not self._sid is None:
             data['sid'] = self._sid
-        resp = requests.post(self.host + route, data=data)
+        resp = self._request(self.host + route, data=data)
         return resp
+
+    def wait_for_connection(self, path=''):
+        print('Waiting for Fritz!Box...', end='', flush=True)
+        start_time = time.time()
+        while True:
+            try:
+                requests.get(self.host)
+            except:
+                pass
+            else:
+                break
+            print('.', end='', flush=True)
+            time.sleep(1)
+        print('')
+        end_time = time.time()
+        return (end_time - start_time) < (3*60)
+
+    def wait_for_reset(self, path=''):
+        print('Waiting for Fritz!Box reset: ', end='', flush=True)
+        start_time = time.time()
+        while True:
+            try:
+                requests.get(self.host)
+            except:
+                break
+            print('-', end='', flush=True)
+            delta = time.time() - start_time
+            if delta > 20:
+                return False
+            time.sleep(1)
+        print('_', end='', flush=True)
+        while True:
+            try:
+                requests.get(self.host)
+            except:
+                pass
+            else:
+                break
+            print('_', end='', flush=True)
+            time.sleep(1)
+        print('-')
+        return True
 
     def reset(self):
         # Assume Pass
         print("Resetting Router")
         relevantDict = self.json['resetWhenPass']
-        requests.post(self.host + relevantDict['route'], data=relevantDict['data'])
-        time.sleep(5)  # Wait before assuming no Pass
-        # try:
-        #     r = requests.get(self.host)
-        # except requests.exceptions.ConnectionError:
-        #     #Router was restarting, skip next step
-        #     return
-        # print("Router had no password, trying alternative")
-        # relevantDict = self.json['restWhenNoPass']
-        # self.call_api(relevantDict)
+        self._request(self.host + relevantDict['route'], data=relevantDict['data'])
+        if self.wait_for_reset():
+            return True
+        print("Router had no password, trying alternative")
+        relevantDict = self.json['restWhenNoPass']
+        self.call_api(relevantDict)
+        return self.wait_for_reset()
+
+    def _request(self, *args, **kwargs):
+        #try:
+        return requests.post(*args, **kwargs)
+        #except:
+        #    pass
 
     def enableExpertMode(self):
         self.login()
@@ -51,22 +110,30 @@ class Connection:
     def wifiSetup(self):
         print("Setting up Wifi Settings")
         relevantDict = self.json['wifiSetup']
-        wifiName = input("Wifi Namen eingeben")
-        relevantDict['data']['wlan:settings/ssid']=wifiName
-        wifiChannel = randint(1,11)
+        wifiName = input("Wifi Namen eingeben: ")
+        relevantDict['data']['wlan:settings/ssid'] = wifiName
+        wifiChannel = randint(1, 11)
         relevantDict['data']['wlan:settings/channel'] = wifiChannel
         self.call_api(relevantDict)
+
+    def getMac(self):
+        route = '/cgi-bin/webcm'
+        query = {
+            'getpage': '../html/de/menus/menu2.html',
+            'var:lang': 'de',
+            'var:menu': 'internet',
+            'var:pagename': 'pppoe',
+            'var:activtype': 'bridge'
+        }
+        if not self._sid is None:
+            query['sid'] = self._sid
+        resp = requests.get(self.host + route, params=query)
+        findings = re.findall('((?:[0-9a-fA-F]{2}[:-]){5}[0-9a-fA-F]{2})', resp.text)
+        findings, *_ = findings
+        print(findings)
 
     def login(self):
         resp = requests.get(self.host + '/login_sid.lua')
         if resp.ok:
             root = ET.fromstring(resp.text)
             self._sid = root.find('SID').text
-        """
-            if self._sid == '0000000000000000':
-                return False
-            else:
-                return True
-        else:
-            return True
-        """
--- main.py
+++ main.py
@@ -1,38 +1,68 @@
 from fritz_postconfig import Connection
-import time
-import subprocess
+import re
 import requests
+import time
 
 
 
-def wait_for_connection(host):
-    print('Waiting for Fritz!Box...', end='', flush=True)
+NAME_DICT = {
+    '7141': 'Fritz!Box Fon WLAN 7141 (UI)',
+    '7170': 'Fritz!Box Fon WLAN 7170 (UI)',
+    'default': 'Fritz!Box Fon WLAN 7141 (UI)',
+}
+
+def main():
+    host = input('Please enter host ({ENTER} for fritz.box): ')
+    if not host:
+        host = '192.168.178.1'
+    host = 'http://' + host
+    # initialize connection
+    fritz = Connection(host)
+    print('Please enter one of the following modelnames: ')
+    for name in NAME_DICT:
+        print('- {:>10}'.format(name))
+    modelname = input('> ')
+    if not fritz.wait_for_connection():
+        print('It is dead, Jim...')
+        return
+    if modelname in NAME_DICT:
+        modelname = NAME_DICT[modelname]
+    else:
+        # Detect fritzbox model
+        print('Reading desc.xml-files...', end='', flush=True)
+        modelname = None
+        time.sleep(10)
+        for file in ('/tr64desc.xml', '/igddesc.xml'):
+            resp = requests.get(host + ':49000' + file)
+            print(resp.text)
+            if resp.ok:
+                result = re.findall('<modelName>(.*?)<\/modelName>', resp.text)
+                modelname, *_ = result
+                for name in NAME_DICT:
+                    if name in modelname:
+                        modelname = NAME_DICT[name]
+                        break
+                else:
+                    modelname = None
+                break
+        print(' DONE')
+    # Manual model entry
+    if modelname is None:
+        print('error finding modelname, using default')
+        modelname = NAME_DICT['default']
+    # Call setup from connection
+    print('Trying model "{}"...'.format(modelname))
+    fritz.model = modelname
+    if fritz.setup():
+        print('Setup correctly')
+        return
+    print('Setup failed')
+
+if __name__ == '__main__':
     while True:
-        returncode = subprocess.call('ping ' + host, stdout=subprocess.PIPE)
-        if returncode == 0:
-            break
-        print('.', end='', flush=True)
-        time.sleep(1)
-    print('')
-
-host = input('Please enter host ({ENTER} for fritz.box): ')
-if not host:
-    host = 'fritz.box'
-wait_for_connection(host)
-host = 'http://' + host
-# Detect fritzbox model
-resp = requests.get(host + ':49000/tr64desc.xml')
-result = re.findall('<modelName>(.*?)<\/modelName>', resp.text)
-modelname, *_ = result
-# Setup connection
-fritz = Connection(host, modelname)
-# reset
-fritz.reset()
-for i in range(100,-1,-1):
-    print(f"\r restarting {i:>3}s left", end="")
-    time.sleep(1)
-print()
-fritz.enableExpertMode()
-fritz.setupInternet()
-fritz.login()
-fritz.wifiSetup()
+        try:
+            main()
+        except KeyboardInterrupt:
+            raise SystemExit
+        except Error as e:
+            print(e)
